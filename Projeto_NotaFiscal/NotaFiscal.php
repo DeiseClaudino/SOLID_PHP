15dc8dbe47e6ce3a5d1f172c9e0aa765f9c3a145
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-06-19 15:59:36 -0300

    Criando Classe NotaFiscal

---
+++ Projeto_NotaFiscal/NotaFiscal.php
@@ -0,0 +1,48 @@
+<?php
+
+class Nota Fiscal{
+  private $id;
+  private $valorBruto;
+  private $impostos;
+
+  public function __construct($valorBruto, $impostos, $id = null)
+  {
+    $this->id = $id;
+    $this->valorBruto = $valorBruto;
+    $this->impostos = $impostos;
+  }
+
+  public function getId()
+  {
+    return $this->id;
+  }
+
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+
+    public function getValorBruto())
+      {
+        return $this->valorBruto;
+      }
+
+
+      public function setValorBruto($valorBruto)
+      {
+          $this->valorBruto = $valorBruto;
+      }
+
+      public function getImpostos()
+      {
+          return $this->impostos;
+      }
+
+
+      public function setImpostos($impostos)
+      {
+          $this->impostos = $impostos;
+      }
+}
