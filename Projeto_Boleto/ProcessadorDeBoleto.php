b780b63f2cbd7fe849f060a0776400e9a7fb78df
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-06-19 18:43:50 -0300

    Criando projeto de processamento de boleto para compreender o encapsulamento

---
+++ Projeto_Boleto/ProcessadorDeBoleto.php
@@ -0,0 +1,23 @@
+<?php
+
+class ProcessadorDeBoleto{
+
+  public function processa($boletos Fatura $fatura)
+  {
+    $total = 0;
+
+    foreach ($boletos as $boleto) {
+      $pagamento = new Pagamento($boleto->getValor(), MeioPagamento::Boleto);
+
+      $fatura->addPagamento($pagamento);
+
+      $total += $boleto->getValor();
+    }
+    if ($total >= $fatura->getValor()) {
+      $fatura->setPago(true);
+
+      return true;
+    }
+    return false;
+  }
+}
