53645af13bbeb6dbc81b79f10c67316ed46ba2c4
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-06-19 18:32:27 -0300

    Finalizando classe Transportadora

--- Projeto_CalculadoraDePrecos/Transportadora.php
+++ Projeto_CalculadoraDePrecos/Transportadora.php
@@ -1,6 +1,12 @@
 <?php
 
-class Transportadora{
-
-  
+class Transportadora implements ServicoDeEntrega
+{
+    public function para($cidade)
+    {
+        if (strtolower($cidade) == "sao paulo") {
+            return 5;
+        }
+        return 10;
+    }
 }
