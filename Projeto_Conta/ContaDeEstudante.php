aa68435debe6c227f313a7cf805ea9de1863b2e3
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-06-20 16:25:50 -0300

    Criando a classe ContaDeEstudante e Instanciando a classe ManipuladorDeSaldo na mesma

--- Projeto_Conta/ContaDeEstudante.php
+++ Projeto_Conta/ContaDeEstudante.php
@@ -1,4 +1,22 @@
 <?php
-class ContaDeEstudante{
-  
+class ContaDeEstudante extends ContaComum
+{
+    private $m;
+    private $milhas;
+
+    public function __construct()
+    {
+        $m = new ManipuladorDeSaldo();
+    }
+
+    public function deposita($valor)
+    {
+        $m->deposita($valor);
+        $this->milhas += (int) $valor;
+    }
+
+    public function getMilhas()
+    {
+        return $this->milhas;
+    }
 }
