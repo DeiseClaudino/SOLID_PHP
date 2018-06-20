b413b9f6b5c0b65c1f0b603e67c4012ec87c76b5
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-06-20 15:23:09 -0300

    Criando o index

---
+++ Projeto_Boleto/index.php
@@ -0,0 +1,18 @@
+<?php
+
+function carregaClasse($classe)
+{
+    require $classe.".php";
+}
+
+spl_autoload_register("carregaClasse");
+
+$boletos = array();
+$boletos = new Boleto(150.0);
+$boletos = new Boleto(350.0);
+
+$fatura = new Fatura("Renan", 500);
+$processador = new ProcessadorDeBoleto();
+$pagou = $processador->processa($boletos, $fatura);
+
+echo $pagou != 0 ? "Pagou!" : "Não Pagou!";
